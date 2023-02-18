71a046a7dad100f658f1e07108a1c32a2f8b2a0c
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2023-02-18 13:30:14 -0800

    router
    logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GameStart from "./Pages/GameStart";
+// import Navigation from './components/Navigation';
+import Wait from './Pages/Wait';
+// import Contact from './Pages/Contact'
+// import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+{/*         <Header /> */}
+{/*         <Home /> */}
+{/*         <About /> */}
+{/*         <Contact/> */}
+
+        <Router>
+{/*           <Navigation> */}
+            <Routes>
+              <Route exact path="/" element={<Wait />} />
+               <Route exact path="/gamestart" element={<GameStart />} />
+{/*               <Route exact path="/contact" element={<Contact />} />  */}
+            </Routes>
+{/*           </Navigation> */}
+        </Router>
+      </div>
+    );
 }
 
 export default App;
---
+++ my-app/src/Pages/GameStart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GameStart() {
+  return (
+    <div>
+      <h1>Welcome to Game!</h1>
+      <p>This is the homepage of my website.</p>
+    </div>
+  );
+}
+
+export default GameStart;
